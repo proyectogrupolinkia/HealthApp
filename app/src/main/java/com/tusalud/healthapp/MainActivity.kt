1d9842eabbe90cd363ad2206a44514f4365a20c7
Javi <javierperezplanas@hotmail.com>
2025-05-25 18:34:57 +0200

    refactor: Coomentamos/.

--- app/src/main/java/com/tusalud/healthapp/MainActivity.kt
+++ app/src/main/java/com/tusalud/healthapp/MainActivity.kt
@@ -1,3 +1,4 @@
+//Punto de entrada de la interfaz de usuario
 package com.tusalud.healthapp
 
 import android.os.Bundle
@@ -9,17 +10,21 @@ import com.tusalud.healthapp.presentation.navigation.AppNavigation
 import com.tusalud.healthapp.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+//@AndroidEntryPoint para habilitar la inyección de dependencias
 
 @AndroidEntryPoint
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Habilita el diseño edge-to-edge
         enableEdgeToEdge()
 
         setContent {
             AppTheme {
 
                 val navController = rememberNavController()
+                // Inicia la navegación principal de la aplicación
                 AppNavigation(navController = navController)
             }
         }
--- app/src/main/java/com/tusalud/healthapp/MyApp.kt
+++ app/src/main/java/com/tusalud/healthapp/MyApp.kt
@@ -1,7 +1,10 @@
 package com.tusalud.healthapp
 
+// Importa la clase base Application del framework Android
 import android.app.Application
+// Impoorta hilt para la inyeccion de dependencias.
 import dagger.hilt.android.HiltAndroidApp
 
+//Punto de entrada global de la aplicación
 @HiltAndroidApp
 class MyApp : Application()
